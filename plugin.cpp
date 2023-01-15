76bd6d7ddc8f25b1ea0ff9a9ab06d96b48b87952
yeahhowaboutnooo <62910188+yeahhowaboutnooo@users.noreply.github.com>
2023-01-15 22:41:40 +0100

    code

--- plugin.cpp
+++ plugin.cpp
@@ -1,12 +1,40 @@
 #include <SkyrimScripting/Plugin.h>
 
-OnInit { logger::info("Hello log from SKSE Starter Kit!"); }
-OnDataLoaded { ConsoleLog("Hello console from SKSE Starter Kit!"); }
+void fail() {
+    logger::error("failed to fix disablePlayerControls overloading the script engine!");
+    return;
+}
 
-EventHandlers {
-    On<RE::TESActivateEvent>([](const RE::TESActivateEvent* event) {
-        auto activated = event->objectActivated->GetBaseObject()->GetName();
-        auto activator = event->actionRef->GetBaseObject()->GetName();
-        ConsoleLog("{} activated {}", activator, activated);
-    });
+void crosshairRefEventsFix() {
+    auto fPlayerCharacter__PickCrosshairReference = REL::RelocationID(39534, 40620).address();
+    if (!fPlayerCharacter__PickCrosshairReference) {
+        fail();
+        return;
+    }
+    uintptr_t offset = 0;
+
+    switch (REL::Module::GetRuntime()) {
+        case REL::Module::Runtime::SE:
+            offset = 96;
+            break;
+        case REL::Module::Runtime::AE:
+            offset = 101;
+            break;
+        default:
+            fail();
+            return;
+    }
+
+    auto injectionPoint = fPlayerCharacter__PickCrosshairReference + offset;
+
+    std::vector<std::uint8_t> nops(6);
+    std::fill(nops.begin(), nops.end(), (std::uint8_t)0x90);
+    REL::safe_write<std::uint8_t>(injectionPoint, nops);
+
+    logger::info("injected Code @ {0:x}", injectionPoint);
+    logger::info("fixed disablePlayerControls overloading the script engine!");
+}
+
+OnInit {
+    crosshairRefEventsFix();
 }
